958c8c564d06705f7d802a8aa1a64b50c6932f2a
Sam M <moralessa978@gmail.com>
2022-02-11 18:50:54 -0700

    added "read" button functionality

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ const form = document.getElementById('addNew');
 const formContainer = document.getElementById('formContainer')
 const overLay = document.getElementById('overLay');
 const submit = document.getElementById('submit');
-const readButtons = document.querySelectorAll('readButton');
+let readButtons = document.querySelectorAll('.readButton');
+
 //Book constructor function
 function Book(title, author, pageCount, read, cover){
     this.Title = title;
@@ -29,7 +30,7 @@ Book.prototype.beInserted = function(arr){
 }
 
 
-function upDateLog(){
+function upDateLogAndButtons(){
     let totalReadCount = myLibrary.filter(function(item){
         return item.Read === true;
     })
@@ -40,15 +41,28 @@ function upDateLog(){
     logTbc.textContent = myLibrary.length;
     logTr.textContent = totalReadCount.length;
     logTnr.textContent = totalNotReadCount.length;
+    readButtons = document.querySelectorAll('.readButton');
 }
 
 function createBook(){
-//    if(coverInput.validity.valid === true){
-//        let newBook = new Book(titleInput, authorInput, pageInput, )
-//    }
-    console.log(
-       typeof(pageInput.value)
-    )
+    console.log(readInput.checked)
+   if(coverInput.validity.valid === true){
+       if(readInput.checked){
+           let newBook = new Book(titleInput.value, authorInput.value, pageInput.value, true, coverInput.value);
+           return newBook;
+       }else{
+            let newBook = new Book(titleInput.value, authorInput.value, pageInput.value, false, coverInput.value);
+            return newBook;
+       }
+   }else{
+    if(readInput.checked){
+        let newBook = new Book(titleInput.value, authorInput.value, pageInput.value, true, '');
+        return newBook;
+    }else{
+         let newBook = new Book(titleInput.value, authorInput.value, pageInput.value, false, '');
+         return newBook;
+    }
+   }
 }
 
 btnAdd.addEventListener('click', () =>{
@@ -61,6 +75,15 @@ overLay.addEventListener('click', () =>{
     overLay.style.display = 'none';
     formContainer.style.display ='none';
 })
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    let b = createBook();
+    addBookToLibrary(b);
+    overLay.style.display = 'none';
+    formContainer.style.display ='none';
+})
+
 
 function addBookToLibrary(book){
     let createdDiv = document.createElement('div');
@@ -104,8 +127,29 @@ function addBookToLibrary(book){
     createdDiv.appendChild(pages);
     createdDiv.appendChild(readToggle);
     cardContainer.appendChild(createdDiv);
-    upDateLog();
+    upDateLogAndButtons();
 }
 
 let greenEggs = new Book('Green Eggs & Ham', 'Dr.Seuss', 62, true, 'images/712nTmzFFRL.jpg')
 addBookToLibrary(greenEggs);
+
+readButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        let bookCard = btn.parentElement;
+        myLibrary.forEach(book => {
+            if(book.BookId == bookCard.id && book.Read == true){
+                book.Read = false;
+                btn.classList.add('readToggleNR');
+                btn.classList.remove('readToggleR');
+                btn.textContent = 'Not Read';
+                upDateLogAndButtons();
+            }else if(book.BookId == bookCard.id && book.Read == false){
+                book.Read = true;
+                btn.classList.add('readToggleR');
+                btn.classList.remove('readToggleNR');
+                btn.textContent = 'Read';
+                upDateLogAndButtons();
+            }
+        })
+    })
+});
